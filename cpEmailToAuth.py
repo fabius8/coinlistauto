2e2a25495817edf9fdb74c3bc902386d74f28bd0
Fang <>
2021-12-01 12:23:48 +0800

    --wip-- [skip ci]

--- cpEmailToAuth.py
+++ cpEmailToAuth.py
@@ -12,7 +12,8 @@ while True:
     string = pyperclip.paste()#读取剪切板内容
     if "@" in string:
         for i in secretjson:
-            if i["Username"] in string:
+            if i["Username"] in string and "HUOBI" in i["Issuer"]:
+                print(i["Issuer"])
                 totp = pyotp.TOTP(i["Secret"])
                 print(string, "[", i["Username"], totp.now(), "]")
                 pyperclip.copy(totp.now()) #重新写入剪切板
