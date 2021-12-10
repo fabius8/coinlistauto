e3f4a39727561efc932097ffec746d33b1a36461
Fang <>
2021-12-10 22:07:32 +0800

    --wip-- [skip ci]

--- clQueueRefresh.py
+++ clQueueRefresh.py
@@ -3,13 +3,14 @@ import datetime
 import time
 
 freshPic = 'detectpic/fresh.png'
-queueUrl_1 = 'https://www.baidu.com'
+queueUrl_1 = 'https://coinlist.co/queue/enter_queue/pstake'
+option1_Endtime = datetime.datetime(2021, 12, 16, 1, 0, 0).replace(microsecond=0)
+
 queueUrl_2 = 'https://www.baidu.com'
-option1_Endtime = datetime.datetime(2021, 12, 5, 23, 50, 0).replace(microsecond=0)
 option2_Endtime = datetime.datetime(2022, 12, 5, 23, 55, 0).replace(microsecond=0)
 
 def openQueueUrl(URL):
-    print("Open Queue URL")
+    print(URL)
     for pos in pyautogui.locateAllOnScreen(freshPic):
         point = pyautogui.center(pos)
         pyautogui.moveTo(point.x + 200, point.y, 2)
@@ -22,14 +23,14 @@ if __name__ == "__main__":
 
     while option1_Endtime > now:
         diff = option1_Endtime - now
-        print(diff, end = '\r')
+        print(queueUrl_1, "( Time Left:", diff, ")", end = '\r')
         now = datetime.datetime.now().replace(microsecond=0)
         time.sleep(1)
     openQueueUrl(queueUrl_1)
 
     while option2_Endtime > now:
         diff = option2_Endtime - now
-        print(diff, end = '\r')
+        print(queueUrl_2, diff, end = '\r')
         now = datetime.datetime.now().replace(microsecond=0)
         time.sleep(1)
     openQueueUrl(queueUrl_2)
