98d45e6d09c77560a69106c8897e724a2f7b356b
Fang <>
2022-04-13 10:10:29 +0800

    --wip-- [skip ci]

--- control/wclient.py
+++ control/wclient.py
@@ -12,7 +12,7 @@ mouse = mouse.Controller()
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-serveIp = "43.135.49.64"
+serveIp = "43.135.86.25"
 tcpURL = 'tcp://' + serveIp + ':5555'
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
